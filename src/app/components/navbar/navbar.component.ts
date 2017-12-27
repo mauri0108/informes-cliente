9d69f1e0fe0a7ab81eff153a7a1c82a68f5a57f6
Mauricio J. Lucero <mjlucero@godoycruz.gov.ar>
2017-12-27 13:47:52 -0300

    servicio que trae los informes para llenar el modal del navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { InformesService } from '../../services/informes.service';
+
+import { Informe } from '../../models/informe';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: []
 })
 export class NavbarComponent implements OnInit {
+  public _informes: Informe[] = [];
 
-  constructor() { }
+  constructor(private _informeService : InformesService) { }
 
   ngOnInit() {
+    this._informeService.getInformes()
+                        .subscribe( res =>  {
+                          this._informes = res.informes;
+                          //this.keys = Object.keys(this._informes);
+                          console.log(res);
+                        },
+                        error =>{
+                          console.log(error);
+                        });
   }
 
 }
