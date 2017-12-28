a6c258d15d530950f3b08345b3afb1b920bf082e
Mauricio J. Lucero <mjlucero@godoycruz.gov.ar>
2017-12-28 13:06:04 -0300

    Se agrego el servicio para traer los informes en el inicio

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InformesService } from '../../services/informes.service';
+
+import { Informe } from '../../models/informe';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class InicioComponent implements OnInit {
+  public _informes: Informe[] = [];
 
-  constructor() { }
+  constructor(private _informeService : InformesService) { }
 
   ngOnInit() {
+    this._informeService.getInformes()
+                        .subscribe( res =>  {
+                          this._informes = res.informes;
+                          //this.keys = Object.keys(this._informes);
+                          console.log(res);
+                        },
+                        error =>{
+                          console.log(error);
+                        });
   }
-
 }
