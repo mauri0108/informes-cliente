d635db2ab233b6c051a26b9058c629ccd0d43676
Mauricio Lucero <mauricio.lucero2014@gmail.com>
2018-01-14 23:42:36 -0300

    llamado al servicio para traer todos los usuarios

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsuariosService } from '../../services/usuarios.service';
+
+import { Usuario } from '../../models/usuario';
+
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styles: []
 })
 export class UsuariosComponent implements OnInit {
+  
+  public _usuarios: Usuario[];
 
-  constructor() { }
+  constructor( private _usuarioService: UsuariosService) { }
 
   ngOnInit() {
-  }
+    this._usuarioService.getUsuarios()
+                        .subscribe( res => {
+                             this._usuarios = res.usuarios; 
+                             console.log(this._usuarios)
+                            },
+                          error => {
+                             console.log(error);
+                          }
 
+    );
+  }
 }
