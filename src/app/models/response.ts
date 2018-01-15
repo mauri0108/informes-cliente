d5068d0325f35650f15a060886f8345bd008a3de
Mauricio Lucero <mauricio.lucero2014@gmail.com>
2018-01-14 23:42:58 -0300

    agregada la interface para manejar las respuestas de la api usuarios

--- src/app/models/response.ts
+++ src/app/models/response.ts
@@ -1,8 +1,16 @@
 import { Informe } from './informe';
+import { Usuario } from './usuario';
 
-export interface InformeResponse{
-  informe? : Informe;
-  informes? : Informe[];
-  message? : string;
+export interface InformeResponse {
+  informe?: Informe;
+  informes?: Informe[];
+  message?: string;
+  error?: string;
+}
+
+export interface UsuarioResponse {
+  usuario?: Usuario;
+  usuarios?: Usuario[];
+  message?: string;
   error?: string;
 }
