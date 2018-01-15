efd63c87c0ccd54a3faf8f8030e3003a4ef291c6
Mauricio Lucero <mauricio.lucero2014@gmail.com>
2018-01-14 23:43:13 -0300

    se agrego la propiedad id

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,12 +1,13 @@
 export class Usuario{
   constructor(
+    public _id: string,
     public nombre: string,
     public apellido: string,
     public email: string,
     public pass: string,
     public role: string,
-    public fechaAlta : string,
-    public fechaBaja : string
+    public fechaAlta: string,
+    public fechaBaja: string
   ){}
 
 }
