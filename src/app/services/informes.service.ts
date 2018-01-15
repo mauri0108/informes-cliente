3ed589d119c4820523905c0e4295dc780d3c669d
Mauricio Lucero <mauricio.lucero2014@gmail.com>
2018-01-14 23:43:35 -0300

    se quitaron los metodos correspondientes al usuario

--- src/app/services/informes.service.ts
+++ src/app/services/informes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs/Observable";
+import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Informe } from '../models/informe';
@@ -8,50 +8,32 @@ import { GLOBAL } from '../global';
 
 import { InformeResponse } from '../models/response';
 
+
 @Injectable()
 export class InformesService {
 
-  constructor(private _http : HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
-  getInformes(){
+  getInformes() {
     return this._http.get< InformeResponse >(GLOBAL.informes);
   }
 
-  getInforme(id:string){
-    let uri =  `${GLOBAL.informe}${id}` ;
+  getInforme(id: string){
+    const uri =  `${GLOBAL.informe}${id}` ;
     return this._http.get< InformeResponse >(uri);
   }
 
-  saveInforme( informe : Informe){
-    let uri = `${GLOBAL.crearInforme}`;
+  saveInforme( informe: Informe) {
+    const uri = `${GLOBAL.crearInforme}`;
 
-    //let headers: HttpHeaders = new HttpHeaders({"Content-Type": "application/json" }); , { headers}
-
-    return this._http.post< InformeResponse >( uri, informe);
-  }
-
-  updateInforme( informe : Informe){
-    let uri = `${GLOBAL.editarInforme}`;
+    // let headers: HttpHeaders = new HttpHeaders({"Content-Type": "application/json" }); , { headers}
 
     return this._http.post< InformeResponse >( uri, informe);
   }
 
-  register( usuario : Usuario ){
-    let uri = `${GLOBAL.registrarse}`;
-    let body = JSON.stringify( usuario );
+  updateInforme( informe: Informe){
+    const uri = `${GLOBAL.editarInforme}`;
 
-    //console.log(body);
-    let headers: HttpHeaders = new HttpHeaders({"Content-Type": "application/json" });
-
-    return this._http.post( uri, body, { headers} );
+    return this._http.post< InformeResponse >( uri, informe);
   }
-
-  login( email : string, pass : string){
-    let uri = `${GLOBAL.login}`;
-    let dataLogin = { email : email, pass : pass};
-    let body = JSON.stringify( dataLogin );
-
-    return this._http.post( uri, body);
-  }
-
 }
