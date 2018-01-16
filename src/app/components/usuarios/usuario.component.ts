7dcb0bc1a33d3894ac6c968cfaeb83ff5dcd2af2
Mauricio J. Lucero <mjlucero@godoycruz.gov.ar>
2018-01-16 16:32:34 -0300

    modificado objecto de usuario con tipo por defecto

--- src/app/components/usuarios/usuario.component.ts
+++ src/app/components/usuarios/usuario.component.ts
@@ -12,7 +12,7 @@ import { UsuariosService  } from '../../services/usuarios.service';
   styles: []
 })
 export class UsuarioComponent implements OnInit {
-  public _usuario: Usuario = new Usuario('', '', '', '', '', '', '', '');
+  public _usuario: Usuario = new Usuario('', '', '', '', '', 'USER', '', '');
   public _id: string;
   public nuevo = true;
   public mensaje: string;
@@ -46,6 +46,7 @@ export class UsuarioComponent implements OnInit {
       this._usuariosService.saveUser(this._usuario)
                            .subscribe(
                             res => {
+                              console.log(res);
                               this._usuario = res.usuario;
                               this._router.navigate(['/admin/usuario', this._usuario._id ]);
                               this.mensaje = res.message;
@@ -61,11 +62,12 @@ export class UsuarioComponent implements OnInit {
       this._usuariosService.updateUser(this._usuario)
           .subscribe(
             res => {
+              console.log(res);
               this._usuario = res.usuario;
               this.mensaje = res.message;
               this.ocultarMensaje(this.mensaje);
             },
-            error =>{
+            error => {
               this.errorMensaje = <string>error.error.split('<br>')[0];
               this.ocultarMensaje(this.errorMensaje);
             }
@@ -76,7 +78,7 @@ export class UsuarioComponent implements OnInit {
   ocultarMensaje(mensaje: any){
     setTimeout( () => {
       this.mensaje = undefined;
-    }, 2000);
+    }, 3000);
   }
 
 }
