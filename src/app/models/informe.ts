515275534ed9af0798c4a1e9574365411107b5f0
Mauricio J. Lucero <mjlucero@godoycruz.gov.ar>
2018-01-18 16:07:46 -0300

    nueva clase informe completo

--- src/app/models/informe.ts
+++ src/app/models/informe.ts
@@ -1,10 +1,20 @@
 import { Item } from './item';
 
-export class Informe{
+export class Informe {
   constructor(
-    public _id:string,
-    public nombre:string,
-    public items : Item[],
-    public creado : string
-  ){}
+    public _id: string,
+    public nombre: string,
+    public items: Item[],
+    public creado: string
+  ) {}
+}
+
+export class InformeCompleto {
+  constructor(
+    public _id: string,
+    public medico: string,
+    public paciente: string,
+    public infDetalle: Informe,
+    public creado: Date
+  ) {}
 }
